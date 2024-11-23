7faf82ee14ed0c1a27ba5e9e580a38eac278b49a
Kim <kim2235@gmail.com>
2024-11-23 14:21:47 +0800

    Update ClientSalesViewerModal.tsx

--- src/components/ClientSalesViewerModalComponent/ClientSalesViewerModal.tsx
+++ src/components/ClientSalesViewerModalComponent/ClientSalesViewerModal.tsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
-import { Sales } from '../../types/Sales'; // Adjust the import path
-
+import { Sales } from '../../types/Sales';
 import { useDispatch, useSelector } from 'react-redux';
-import { RootState, AppDispatch } from "../../redux/store";
+import {RootState, AppDispatch, store} from "../../redux/store";
 import Button from "../ButtonComponent/Button";
 import {selectSalesItemsBySaleId,updateServiceStatus} from "../../redux/slices/salesItemsSlice";
-import {selectServiceById} from "../../redux/slices/serviceSlice";
 import {Service} from "../../types/Service";
 import {Product} from "../../types/Product";
 import {addOrUpdateProduct} from "../../redux/slices/productSlice";
+import {selectPackageById} from "../../services/packageServices";
+import TextView from "../TextViewComponent/TextView";
+
 interface SalesDetailsModalProps {
     isOpen: boolean;
     onClose: () => void;
@@ -47,7 +48,6 @@ const ClientSalesViewerModal: React.FC<SalesDetailsModalProps> = ({
             status: Number(inputValue) >= Number(salesDetails.balance) ? 'paid' : 'unpaid',
             payment: Number(inputValue) || 0, // Update the payment with the input value
         };
-
         onSave(updatedSales);
         onClose();
     };
@@ -78,10 +78,14 @@ const ClientSalesViewerModal: React.FC<SalesDetailsModalProps> = ({
             });
         });
     };
+    const getPackageById = (id: string) => {
+        const state = store.getState();
+        return selectPackageById(state, id);
+    };
     return (
         <div className={`fixed inset-0 flex items-start justify-center z-50`}>
             <div className={`absolute inset-0 bg-black opacity-50`} />
-            <div className={`bg-white rounded-lg shadow-lg p-6 mt-20 z-10`}>
+            <div className={`bg-white rounded-lg shadow-lg p-6 mt-20 z-10 w-48.5p`}>
                 <h2 className={`text-lg font-bold mb-4`}>Sale Details</h2>
                 <div className={`mb-4`}>
                     <h3 className={`font-semibold`}>Sale ID: {salesDetails.id}</h3>
@@ -90,30 +94,67 @@ const ClientSalesViewerModal: React.FC<SalesDetailsModalProps> = ({
                 </div>
                 <h3 className={`font-semibold mb-2`}>Services:</h3>
                 <div className={`overflow-y-auto max-h-60`}>
-                    <ul className={`list-disc list-inside mb-4`}>
+                    <ul className={`list-none list-inside mb-4`}>
                         Service/Packages that this client avail:
                         {salesItems.map((service) => (
-                            <li key={service.id} className={`m-2`}>
-                                <span className={`font-medium`}>{service.name}</span> -
-                                ${service.cost != null ? service.cost : '0.00'} : {service.description}
-                                <span
-                                    onClick={() => !service.isDone && handleMarkAsDone(service.id,service.serviceId)} // Only trigger if not done
+                            <li key={service.id} className={`m-2 border border-emerald-300 p-2`}>
+                                <div>
+                                    <span className={`font-medium`}>
+                                        <TextView text={`Name: `}></TextView>
+                                    </span>
+                                    <span>
+                                        <TextView text={service.name}></TextView>
+                                    </span>
+                                    <span
+                                    onClick={() => !service.isDone && handleMarkAsDone(service.id, service.serviceId)} // Only trigger if not done
                                     className={`ml-2 cursor-pointer text-blue-500 ${service.isDone ? 'line-through text-gray-400 cursor-not-allowed' : ''}`}
                                 >
                                 {service.isDone ? 'Done' : 'Mark this Done'}
                                 </span>
+                                </div>
+                                <div>
+                                    <span className={`font-medium`}>
+                                        <TextView text={`Description: `}></TextView>
+                                    </span>
+                                    <span className={`font-light`}>
+                                        <TextView text={service.description}></TextView>
+                                    </span>
+                                </div>
+                                <div>
+                                     <span className={`font-medium`}>
+                                        <TextView text={`Cost: `}></TextView>
+                                    </span>
+                                    <span className={`font-light`}>
+                                        $ <TextView text={service.cost != null ? service.cost.toString() : '0.00'}></TextView>
+                                    </span>
+                                </div>
+
+
+                                {!service.category ? (
+                                    getPackageById(service.serviceId.toString()).map((pkg) => (
+                                        <div className={``}>
+                                            <TextView text={`Package Inclusion: `} className={`font-semibold`}></TextView>
+                                            <ul key={pkg.id} className={``}>
+                                                {pkg.services.map((selected) => (
+                                                    <li key={selected.id}>- {selected.name}</li>
+                                                ))}
+                                            </ul>
+                                        </div>
+                                    ))
+                                ) : null}
                             </li>
                         ))}
                     </ul>
 
                 </div>
-                <h3 className={`font-semibold`}> Total: ${salesDetails.total ? salesDetails.total.toFixed(2) : '0.00'}</h3>
+                <h3 className={`font-semibold`}> Total:
+                    ${salesDetails.total ? salesDetails.total.toFixed(2) : '0.00'}</h3>
 
                 {salesDetails.status === 'paid' && (
                     <div className={`mt-4`}>
-                    <label className={`block mb-2 font-semibold`} htmlFor="inputField">
-                                Paid Amount:
-                            </label>
+                        <label className={`block mb-2 font-semibold`} htmlFor="inputField">
+                            Paid Amount:
+                        </label>
                             <input
                                 type="text"
                                 id="inputField"
